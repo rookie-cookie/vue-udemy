29f0efd15d17d7e8bc43f9cc06b66c9b296c2248
Jona Reyes <jonamreyes@gmail.com>
2021-04-05 11:00:01 -0400

    setting up reuseable character component

--- appplications/starbase/src/App.vue
+++ appplications/starbase/src/App.vue
@@ -1,11 +1,14 @@
 <template lang="html">
   <div id="app">
     <h3>{{title}}</h3>
-    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
+    <!-- <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button> -->
+    <Character/>
   </div>
 </template>
 
 <script>
+import Character from './components/Character.vue'
+
 export default {
   name: 'app',
   data() {
@@ -13,14 +16,8 @@ export default {
       title: 'Generate Your Team'
     }
   },
-  methods: {
-    fetchCharacter(){
-      fetch('https://swapi.dev/api/people/1/', {
-        method: 'GET'
-      })
-      .then(response => response.json())
-      .then(json => console.log(json))
-    }
+  components:{
+    Character
   }
 }
 </script>
---
+++ appplications/starbase/src/components/Character.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="">
+    {{character}}
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      character: {
+
+      }
+    }
+  },
+  methods:{
+    fetchCharacter(){
+      fetch('https://swapi.dev/api/people/', {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => this.character = json)
+    }
+  },
+  created(){
+    this.fetchCharacter()
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
