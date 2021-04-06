bfc8525060b9cc2034883c2f5c54abe534aa8a9a
Jona Reyes <jonamreyes@gmail.com>
2021-04-06 10:39:25 -0400

    fetch data from dynamic route

--- appplications/starbase-router/src/components/Data.vue
+++ appplications/starbase-router/src/components/Data.vue
@@ -1,6 +1,8 @@
 <template lang="html">
   <div class="">
     Type: {{type}}
+
+    {{items}}
   </div>
 </template>
 
@@ -8,16 +10,30 @@
 export default {
   data(){
     return {
-      type: this.$route.params.type //$route bc of vue router, params = query strings
+      type: this.$route.params.type, //$route bc of vue router, params = query strings
+      items: []
     }
   },
   watch: {  //will keep track of any changes to the route and will run a function everytime it changes
-    '$route': 'change' //whenever route changes, it will call the method 'change'
+    '$route': 'fetchItems' //whenever route changes, it will call the method 'fetchItems'
   },
   methods: {
-    change(){
+    fetchItems(){  //populate items to the array of data from the api
+      this.items = []
       this.type = this.$route.params.type
+      let initial_ids = [1,13,14]
+      for (let i in initial_ids){
+        let id = initial_ids[i]
+        fetch(`https://swapi.dev/api/${this.type}/${id}/`, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => this.items.push(json))
+      }
     }
+  },
+  created(){  //loads data on load
+    this.fetchItems()
   }
 }
 </script>
