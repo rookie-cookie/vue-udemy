033303dab3dc1648bd922278560c4ad6b9ab07d5
Jona Reyes <jonamreyes@gmail.com>
2021-04-06 09:44:21 -0400

    setting up router for starbase router

---
+++ appplications/starbase-router/src/App.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div id="app">
+    <h3>Starbase Router</h3>
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ appplications/starbase-router/src/components/Data.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    Type: {{type}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      type: this.$route.params.type //$route bc of vue router, params = query strings
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
