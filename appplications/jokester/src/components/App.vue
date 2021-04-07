a45e70c96d9628ddcb11d35b42c30987e1e0a251
Jona Reyes <jonamreyes@gmail.com>
2021-04-07 10:23:16 -0400

    added Joke component and styling

--- appplications/jokester/src/components/App.vue
+++ appplications/jokester/src/components/App.vue
@@ -2,19 +2,33 @@
   <div id="app">
     <h3>Got Jokes?</h3>
     <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
-    <div class="">
-      {{$store.state}} <!-- store is globally available with $ w/in the component bc it is in the vue instance -->
+    <br>
+    <br>
+    <div class="col-md-12">
+      <Joke
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        key="index"
+      />
     </div>
+    <!-- <div class="">
+      {{$store.state}}  -->
+      <!-- store is globally available with $ w/in the component bc it is in the vue instance -->
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
   methods: mapActions([
     'initJokes'
-  ])
+  ]),
+  components: {
+    Joke
+  }
 }
 </script>
 
