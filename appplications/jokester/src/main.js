85bf1ba8aff7ee0329541b41d0c754b66009676f
Jona Reyes <jonamreyes@gmail.com>
2021-04-07 09:32:18 -0400

    setting up mutations for the store

--- appplications/jokester/src/main.js
+++ appplications/jokester/src/main.js
@@ -3,6 +3,8 @@ import App from './components/App.vue'
 
 import store from './store'  //no need to specify index.js file bc it is automatically found as the default javascript file
 
+// store.commit('INIT_JOKES', [{test: 'test_joke'}, {test2: 'test_joke2'}])
+
 new Vue({
   el: '#app',
   store, //pass in the store to the vue instance
