c13b7c73c14784c703bcb85ec3fd86e7af2ad0e5
Jona Reyes <jonamreyes@gmail.com>
2021-04-08 09:34:29 -0400

    added firebase authentication

---
+++ appplications/turnout/src/firebaseApp.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyBAhLsQopW9jdlkuGPp2HVhyoVknH4CzaU",
+  authDomain: "turnout-a517c.firebaseapp.com",
+  projectId: "turnout-a517c",
+  storageBucket: "turnout-a517c.appspot.com",
+  messagingSenderId: "959665258500",
+  appId: "1:959665258500:web:2b691a531c2621b4853551",
+  measurementId: "G-J25Y8E6HNC"
+};
+
+export const firebaseApp = firebase.initializeApp(config)
