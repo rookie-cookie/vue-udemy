0cdf4776334d2d7692164f2a3c9cd0c6a61cce4e
Jona Reyes <jonamreyes@gmail.com>
2021-04-08 10:34:55 -0400

    setup vuex

--- appplications/turnout/src/components/Dashboard.vue
+++ appplications/turnout/src/components/Dashboard.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div class="">
     Dashboard Component
+    {{$store.state}}
   </div>
 </template>
 
--- appplications/turnout/src/main.js
+++ appplications/turnout/src/main.js
@@ -5,6 +5,8 @@ import { firebaseApp } from './firebaseApp'
 
 Vue.use(VueRouter)
 
+import store from './store'
+
 import Dashboard from './components/Dashboard.vue'
 import Signin from './components/Signin.vue'
 
@@ -21,6 +23,7 @@ const router = new VueRouter({
 
 firebaseApp.auth().onAuthStateChanged(user => {
   if (user) {
+    store.dispatch('signIn', user)
     router.push('/dashboard')
   } else {
     router.replace('/signin')
@@ -30,5 +33,6 @@ firebaseApp.auth().onAuthStateChanged(user => {
 new Vue({
   el: "#app",
   router,
+  store,
   render: h => h(App)
 })
