21c5b98d59b4cd55f3ccb47ad163b65b2f5fc9f6
Jona Reyes <jonamreyes@gmail.com>
2021-04-09 11:09:05 -0400

    added events form

--- appplications/turnout/src/components/Dashboard.vue
+++ appplications/turnout/src/components/Dashboard.vue
@@ -2,12 +2,17 @@
   <div class="">
     <h3>Events Dashboard</h3>
     <button type="button" class="btn btn-danger btn-sm signout-btn" @click="signOut" >Sign Out</button>
+    <hr>
+    <AddEvent/>
+    <hr>
     {{$store.state}}
+
   </div>
 </template>
 
 <script>
 import { firebaseApp } from '../firebaseApp'
+import AddEvent from './AddEvent.vue'
 
 export default {
   methods: {
@@ -15,6 +20,9 @@ export default {
       this.$store.dispatch('signOut')
       firebaseApp.auth().signOut()
     }
+  },
+  components:{
+    AddEvent
   }
 
 }
--- appplications/turnout/src/firebaseApp.js
+++ appplications/turnout/src/firebaseApp.js
@@ -13,3 +13,4 @@ const config = {
 };
 
 export const firebaseApp = firebase.initializeApp(config)
+export const eventsRef = firebaseApp.database().ref().child('events')
